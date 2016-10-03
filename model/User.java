f410c04e9d2ed97e76f220b7533630343051aa59
Shivani Upadhayay <supadhayay3@gatech.edu>
2016-10-03 12:21:38 -0400

    Profile Page

--- model/User.java
+++ model/User.java
@@ -1,9 +1,18 @@
 package model;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by Shivani on 10/3/16.
  */
+
 public class User extends AuthorizedUser {
+    protected enum WaterType;
+    protected enum WaterCondition;
+    protected static int counter = 0;
+    protected int num = 0;
 
     public User(String name, String id, String password) {
         this.name = name;
@@ -11,8 +20,15 @@ public class User extends AuthorizedUser {
         this.password = password;
     }
 
-    public void submitReport() {
+    public void submitReport(WaterType, WaterCondition) {
         // code
+        this.WaterType = WaterType;
+        this.WaterCondition = WaterCondition;
+        counter++;
+        num = counter;
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        Calendar calobj = Calendar.getInstance();
+        System.out.println(df.format(calobj.getTime()));
     }
 
     public void viewWaterSources() {
