84203a6d7ca1151bb44468d5b8e85bc2aee557c9
Shivani Upadhayay <supadhayay3@gatech.edu>
2016-10-03 13:27:59 -0400

    Added address and title info for Authorized user

--- model/User.java
+++ model/User.java
@@ -3,41 +3,37 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-package com.gpstracker;
 /**
  * Created by Shivani on 10/3/16.
  */
 
 public class User extends AuthorizedUser {
-    protected enum WaterType;
-    protected enum WaterCondition;
     protected static int counter = 0;
     protected int num = 0;
-    protected String emailaddress;
-    protected String homeaddress;
-    protected String title;
+    //protected WaterType type;
+    //protected WaterCondition condition;
+    //protected String emailaddress;
+    //protected String homeaddress;
+    //protected String title;
 
-    public User(String name, String id, String password, String emailaddress, String homeaddress, String title) {
+    public User(String name, String id, String password) {
         this.name = name;
         this.id = id;
         this.password = password;
-        this.emailaddress = emailaddress;
-        this.homeaddress = homeaddress;
-        this.title = title;
+        //this.emailaddress = emailaddress;
+        //this.homeaddress = homeaddress;
+        //this.title = title;
     }
 
-    public void submitReport(WaterType, WaterCondition) {
+    public void submitReport(WaterType type, WaterCondition condition) {
         // code
-        this.WaterType = WaterType;
-        this.WaterCondition = WaterCondition;
-        counter++;
-        num = counter;
-        //PositionService positionService = PlatformFactory.getPlatform().getPositionService();
-        //Position position = positionService.getPosition();
-        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-        Calendar calobj = Calendar.getInstance();
-        System.out.println(df.format(calobj.getTime()));
-        //System.out.println("Current GPS position: " + position.getLatitude() + "," + position.getLongitude());
+        //this.WaterType = WaterType;
+        //this.WaterCondition = WaterCondition;
+        //counter++;
+        //num = counter;
+        //DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        //Calendar calobj = Calendar.getInstance();
+        //System.out.println(df.format(calobj.getTime()));
     }
 
     public void viewWaterSources() {
