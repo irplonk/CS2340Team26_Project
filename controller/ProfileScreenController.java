9c2f72e536d6cd069b4b446c6ed3c93ebed76092
Shivani Upadhayay <supadhayay3@gatech.edu>
2016-10-03 15:08:20 -0400

    mainfx for profile screen

---
+++ controller/ProfileScreenController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import fxapp.MainFXApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.AuthorizedUser;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * The controller for the profile page screen
+ * Created by Shivani on 10/3/16
+ * @version 1.0
+ */
+
+public class ProfileScreenController {
+
+    /** reference back to mainApplication if needed */
+    private MainFXApplication mainApplication;
+
+    private Stage profileScreenStage;
+
+    private final ObservableList<AuthorizedUser> data =
+            FXCollections.observableArrayList(
+                    new AuthorizedUser("Jacob", "Smith", "jacob.smith@example.com"),
+            );
+
+
+}
