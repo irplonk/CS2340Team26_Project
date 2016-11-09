ca4dce95b56cc1e4470ffc2ee627fe40da6ac2dc
Shivani Upadhayay <supadhayay3@gatech.edu>
2016-11-09 08:40:25 -0500

    history report additions

--- controller/HistoryReportInputController.java
+++ controller/HistoryReportInputController.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 import model.HistoryReport;
 import model.UserType;
 
+import java.util.Hashtable;
+
 /**
  * Created by Shivani Upadhayay on 11/1/2016.
  */
@@ -21,7 +23,12 @@ public class HistoryReportInputController {
 
     private Stage viewHistoryReportInputStage;
 
+    private String month;
+    private Double ppmval;
+    private HistoryReport historyReport;
+
     private HistoryReport backingArray[] = new HistoryReport[10];
+    Hashtable<String, Double> historicalReports = new Hashtable<String, Double>();
 
     @FXML
     private ComboBox<String> ppm = new ComboBox<>();
@@ -69,7 +76,7 @@ public class HistoryReportInputController {
         mainApplication = main;
     }
 
-    public void addReports() {
+/*    public void addReports() {
         HistoryReport rep1 = new HistoryReport("Kevin", "Atlanta, GA", 22.4, 34.5, 300, "virus", "May", 2016);
         HistoryReport rep2 = new HistoryReport("Isabella", "Chicago, IL", 34.8, 78.0, 400, "contaminant", "May", 2016);
         HistoryReport rep3 = new HistoryReport("Shivani", "New York, NY", 22.4, 38.6, 250, "contaminant", "May", 2016);
@@ -82,7 +89,7 @@ public class HistoryReportInputController {
         backingArray[3] = rep4;
         backingArray[4] = rep5;
         backingArray[5] = rep6;
-    }
+    }*/
 
     /**
      * Method to search for reports in certain year
@@ -104,8 +111,43 @@ public class HistoryReportInputController {
         return false;
     }
 
+    /**
+    * Private method to add data to hash table
+     * Gives default values of 0.0 then tries to
+     * add a single data point for each month
+     */
+    private void addData() {
+        historicalReports.put("Jan", 0.0);
+        historicalReports.put("Feb", 0.0);
+        historicalReports.put("Mar", 0.0);
+        historicalReports.put("Apr", 0.0);
+        historicalReports.put("May", 0.0);
+        historicalReports.put("June", 0.0);
+        historicalReports.put("July", 0.0);
+        historicalReports.put("Aug", 0.0);
+        historicalReports.put("Sep", 0.0);
+        historicalReports.put("Oct", 0.0);
+        historicalReports.put("Nov", 0.0);
+        historicalReports.put("Dec", 0.0);
+        if (doesDataMatch()) {
+            for (String key : historicalReports.keySet()) {
+                if (key.equals(historyReport.getMonth())) {
+                    historicalReports.put(key, historyReport.getPPM());
+                    while (historicalReports.values().size() > 1) {
+                        double sum = 0;
+                        for (int j = 0; j < historicalReports.values().size(); j++) {
+                            sum += j;
+                        }
+                        double average = (sum / (historicalReports.values().size()));
+                        //add this average value to the chart for select month if data is equal
+                    }
+                }
+            }
+        }
+    }
 
-    private void collectData() {
+
+/*    private void collectData() {
         if (doesDataMatch()) {
             for (int i = 0; i < backingArray.length; i++) {
                 if (backingArray[i].getPpmType() == "contaminant") {
@@ -131,5 +173,5 @@ public class HistoryReportInputController {
                 }
             }
         }
-    }
+    }*/
 }
--- model/HistoryReport.java
+++ model/HistoryReport.java
@@ -19,61 +19,117 @@ public class HistoryReport extends Report {
         this.PPM = PPM;
     }
 
+    /**
+     * Method to get location
+     * @return string location
+     */
     @Override
     public String getLocation() {
         return this.location;
     }
 
+    /**
+     * Method to get latitude
+     * @return double latitude
+     */
     @Override
     public double getLatitude() {
         return this.latitude;
     }
 
+    /**
+     * Method to get longitude
+     * @return double longitude
+     */
     @Override
     public double getLongitude() {
         return this.longitude;
     }
 
+    /**
+     * Method to get ppm value
+     * @return ppm
+     */
     public double getPPM() {
         return this.PPM;
     }
 
+    /**
+     * Method to get year
+     * @return int year
+     */
     public int getYear() {
         return this.year;
     }
 
+    /**
+     * Method to get ppm value
+     * @return ppm
+     */
     public String getPpmType() {
         return this.ppmType;
     }
 
+    /**
+     * Method to get month
+     * @return String month
+     */
     public String getMonth() {
         return this.month;
     }
 
+    /**
+     * Method to set location
+     * @param location the location to be inputted
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * Method to set latitude
+     * @param latitude the latitude to be inputted
+     */
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * Method to set longitude
+     * @param longitude the longitude to be inputted
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     * Method to set ppm
+     * @param ppm the ppm to be inputted
+     */
     public void setPPM(double ppm) {
         this.PPM = ppm;
     }
 
+    /**
+     * Method to set year
+     * @param year the year to be inputted
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     * Method to set ppm type
+     * @param ppmType the ppmType to be inputted
+     */
     public void setPpmType(String ppmType) {
         this.ppmType = ppmType;
     }
 
+    /**
+     * Method to set month
+     * @param month the month to be inputted
+     */
     public void setMonth(String month) {
         this.month = month;
     }
